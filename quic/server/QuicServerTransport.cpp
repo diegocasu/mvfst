3f647cf129cfcc7ca3a422814ee852cd55ce36dd
Diego Casu <d.casu3@studenti.unipi.it>
2022-05-17 19:28:14 +0200

    Update write looper in next iteration when migrating server transport

--- quic/server/QuicServerTransport.cpp
+++ quic/server/QuicServerTransport.cpp
@@ -483,7 +483,7 @@ void QuicServerTransport::onNetworkSwitch(
       if (shouldWriteData(*conn_) == WriteDataReason::NO_WRITE) {
         sendServerMigrationFrame(*serverConn_, ServerMigratedFrame());
       }
-      updateWriteLooper(true);
+      updateWriteLooper(false);
       return;
   }
   folly::assume_unreachable();
